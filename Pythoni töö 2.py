a918fa15471ef4e10d13c64fb398f43520a37ea8
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-03-02 10:58:32 +0200

    Update Pythoni töö 2.py

--- Pythoni töö 2.py
+++ Pythoni töö 2.py
@@ -2,6 +2,20 @@
 #Pythoni töö 2
 #02.03.2022
 
+#õunad
+'''
+import random
+
+lumivalgeke = 14 
+pöial = int(input("Mitu pöialpoissi tahavad õuna?: "))
+for i in range(pöial):
+    oun = random.randint(1,2)
+    print(oun)
+    lumivalgeke = lumivalgeke - oun
+print("Lumivalgekesele jäi: ",lumivalgeke)
+'''
+        
+
 #male
 '''
 arv = int(input("Sisestage täisarv: "))
@@ -12,7 +26,7 @@ i = 1
 while i <= arv:
     nisuterad *= 2
     i+=1
-print(f"Nisuteri on {arv}, ruudu eest: {nisuterad}")
+print(f"Nisuteri on {arv} ruudu eest: {nisuterad}")
 '''
 
 #täringud
@@ -49,7 +63,3 @@ aratus = int(input("Mitu korda peaks äratus helisema?: "))
 for i in range(aratus):
     print("Tõuse ja sära!")
 '''   
-    
-
-    
-    
