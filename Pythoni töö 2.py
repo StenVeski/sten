706f523ba4d95b3c1d233889383f76854731b047
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-03-02 10:37:28 +0200

    Add files via upload

---
+++ Pythoni töö 2.py
@@ -0,0 +1,55 @@
+#Sten Veski
+#Pythoni töö 2
+#02.03.2022
+
+#male
+'''
+arv = int(input("Sisestage täisarv: "))
+
+nisuterad = 1
+
+i = 1
+while i <= arv:
+    nisuterad *= 2
+    i+=1
+print(f"Nisuteri on {arv}, ruudu eest: {nisuterad}")
+'''
+
+#täringud
+'''
+import random
+
+taringud = int(input("Sisestage täringute arv: "))
+
+for i in range(taringud):
+    print(random.randint(1,6))
+'''
+
+#murelikud lapsevanemad
+'''
+ringid = int(input("Mitu ringi läbisid?: "))
+
+porgandid = 0
+
+
+ring = 1
+while ring <= ringid:
+    if i%2==0:
+        porgandid+=1
+    ring += 1
+print(f"Porgandite koguarv on: {porgandid} ")
+'''
+
+
+        
+#äratus
+'''
+aratus = int(input("Mitu korda peaks äratus helisema?: "))
+
+for i in range(aratus):
+    print("Tõuse ja sära!")
+'''   
+    
+
+    
+    
