f77e5137eb870a5033a13fcee41d8f3ed6536626
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-03-14 20:45:08 +0000

    Update Graafiline python iseseisev.py

--- Graafiline python iseseisev.py
+++ Graafiline python iseseisev.py
@@ -7,10 +7,23 @@ import turtle
  
 
 aken = turtle.Screen()
-aken.setup(300,300)
+aken.setup(500,500)
 aken.title("Iseseisevtöö")
+varv = ('blue')
 
 kk = turtle.Turtle()
+for i in range(3):
+    kk.color(varv)
+    kk.forward(100)
+    kk.left(120)
+    kk.forward(100)
+    
+for i in range(3):
+    kk.color(varv)
+    kk.left(120)
+    kk.forward(100)
+    kk.left(120)
+    
 
 
 
