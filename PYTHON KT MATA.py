469747eb455afb2ec1f9bbf0144b62da7e52251c
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-03-14 20:02:53 +0000

    Update PYTHON KT MATA.py

--- PYTHON KT MATA.py
+++ PYTHON KT MATA.py
@@ -6,22 +6,24 @@ from random import randint
 
 mitu = int(input("Mitu korda soovite mängu mängida?: "))
 play = 1
-count = 1
-while play == 1:
+count = 0
+for i in range(mitu):
     x = randint(0,10)
     y = randint(0,10)
     
-    print("Teie tehe on: ",x, "*", y)
+    tehe = print("Teie tehe on: ",x, "*", y)
     a = int(input("Vastus: "))
     
     if a == x*y:
         print("ÕIGE")
-    else:
-        print("VALE. Proovi uuesti:",)
         count +=1
+    else:
+        print("VALE")
+        print(f"Said {count} õiget vastust")
+        
     
         play = int(input("1 = Mängi uuesti \n0 = Välju \n:"))
         if play == 0:
+            print(f"Said {count} õiget vastust")
             print("Aitäh mängimast!")
-   
         
