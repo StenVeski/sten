f17471b8f479016016f852f0a86c64cfd521db48
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-03-15 10:51:07 +0200

    Update PYTHON KT MATA.py

--- PYTHON KT MATA.py
+++ PYTHON KT MATA.py
@@ -4,26 +4,33 @@
 
 from random import randint
 
-mitu = int(input("Mitu korda soovite mängu mängida?: "))
-play = 1
+
 count = 0
-for i in range(mitu):
-    x = randint(0,10)
-    y = randint(0,10)
-    
-    tehe = print("Teie tehe on: ",x, "*", y)
-    a = int(input("Vastus: "))
-    
-    if a == x*y:
-        print("ÕIGE")
-        count +=1
-    else:
-        print("VALE")
-        print(f"Said {count} õiget vastust")
-        
-    
-        play = int(input("1 = Mängi uuesti \n0 = Välju \n:"))
-        if play == 0:
+play = 1
+
+while play ==1:
+    mitu = int(input("Mitu korda soovite mängu mängida?: "))
+    for i in range(mitu):
+        x = randint(0,10)
+        y = randint(0,10)
+
+        print("Teie tehe on: ",x, "*", y)
+        a = int(input("Vastus: "))
+               
+            
+        if a == x*y:
+            print("ÕIGE")
+            count +=1
+        else:
+            print("VALE")
             print(f"Said {count} õiget vastust")
-            print("Aitäh mängimast!")
+            
+            
+    play = int(input("1 = Mängi uuesti \n0 = Välju \n:"))
+    if play == 1:
+        print("Teie tehe on: ",x, "*", y)
+        a = int(input("Vastus: "))
+    else:
+        print(f"Said {count} õiget vastust")
+        print("Aitäh mängimast!")
         
