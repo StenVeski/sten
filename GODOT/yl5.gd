630a65a690ec1de947c0f62b57ffcc90702520ff
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-04-14 09:29:16 +0300

    Update yl5.gd

--- GODOT/yl5.gd
+++ GODOT/yl5.gd
@@ -1,16 +1,31 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+		var tunnitasu = 1.5
+		var tasu = 0
+		var tootunnid = 40
+		if tootunnid == 40:
+			tasu = tunnitasu * tootunnid
+			print(tasu)
+		else:
+			tasu = 40*tunnitasu+(tootunnid-40)*1.5*tootunnid
+			print(tasu)
+			
+			
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		var punktid = [7,28,64,51,81,40,21,73,34,98,39,17,33,85,35,44] 
+		var keskmine = 0
+		for i in punktid:
+			keskmine += i
+		keskmine = keskmine/len(punktid)
+		print(punktid)
+		print(keskmine)
+		for i in punktid: 
+			if i >= 90:
+				print(i,"p - 5")
+			elif i >= 75:
+				print(i,"p - 4")
+			elif i >= 50:
+				print(i,"p - 3")
+			elif i <= 50:
+				print(i,"p - 2")	
