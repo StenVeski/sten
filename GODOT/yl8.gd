365ad24ba0e0ba149e8460e191641a9ee58f03ac
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-04-18 13:56:15 +0300

    Update yl8.gd

--- GODOT/yl8.gd
+++ GODOT/yl8.gd
@@ -1,10 +1,27 @@
 extends Node
 
+var score = 0
+var score1 = 0 
+var kasutaja = ""
+var AI = ["Kivi","Paber","Käärid"]
 
-func _on_Kivi_pressed():
+func _ready():
+	pass
+	
+
+func _on_kivi_pressed():
+	kasutaja = "Kivi"
+	var random = RandomNumberGenerator.new()
+	random.randomize()
+	var dmg = round(rand_range(0,2))
+	if AI[dmg] == "Kivi":
+		
+	
+	
 	print("Kivi")
-func _on_Paber_pressed():
+func _on_paber_pressed():
 	print("Paber")
-func _on_Kaarid_pressed():
+func _on_kaarid_pressed():
 	print("Käärid")
-
+func _on_uus_pressed():
+	get_tree().reload_current_scene()
