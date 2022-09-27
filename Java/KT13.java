3fd6db176076780def2a08dd8ffdcce096c65585
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-09-27 14:27:10 +0300

    Update KT13.java

--- Java/KT13.java
+++ Java/KT13.java
@@ -16,7 +16,7 @@ public class KT13
         {
             for (int j = 1; j <= i; j++)
             {
-                System.out.print(j+"*");
+                System.out.print("*");
             }
             System.out.println();
         }
