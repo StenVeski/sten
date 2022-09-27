2b80d85fa61d5d31e6a1fb0d68dd83217b6caf47
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-09-27 14:31:52 +0300

    Add files via upload

---
+++ Java KT/KT13.java
@@ -0,0 +1,25 @@
+package harjutused;
+import java.util.Scanner;
+
+public class KT13 
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sisestage täisarv: ");
+        
+        int rows = scanner.nextInt();
+        
+        System.out.println("Teie kolmnurk: ");
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+      scanner.close();
+    }
+}
