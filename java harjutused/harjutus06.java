841a156fb19c6eff854a195add37f82bdf06e7ed
StenVeski <99240869+StenVeski@users.noreply.github.com>
2022-10-05 11:39:11 +0300

    Update harjutus06.java

--- java harjutused/harjutus06.java
+++ java harjutused/harjutus06.java
@@ -1,19 +1,21 @@
-// Sten Veski Harjutus 6
+// Sten Veski IT21 Harjutus 6 //
 
+package harjutused;
 import java.util.Scanner;
 import java.io.PrintWriter;
 import java.io.*;
 import java.util.ArrayList;
 
-public class harjutus06 {
+public class harjutus6 {
     public static void main(String[] args) {
+    	
         ArrayList<Integer> list = new ArrayList<Integer>();
-        Scanner scanner = new Scanner (System.in);
+        Scanner sc = new Scanner (System.in);
         int a = 1;
 
         while(a == 1) {
             System.out.println("Sisestage arv: ");
-            String nr = scanner.nextLine();
+            String nr = sc.nextLine();
 
         if(nr == " ") {
             break;
@@ -24,13 +26,13 @@ public class harjutus06 {
         }
         int kokku = list.size();
         int plus = list.stream().mapToInt(Integer::intValue).sum();
-        int keskmine = plus/kokku;
+        int kesk = plus/kokku;
 
     try {
             PrintWriter bw = new PrintWriter("nr.txt");
             bw.println("Arvud: "+ list);
             bw.println("Summa: "+ plus);
-            bw.println("Keskmine: "+ keskmine);
+            bw.println("Keskmine: "+ kesk);
             bw.close();
             System.out.println("File loodud! ");
 
