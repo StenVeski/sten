43c63d798da9b8b5f367b86673faa7a25b1ae1fd
StenVeski <99240869+StenVeski@users.noreply.github.com>
2023-02-14 15:31:31 +0200

    Add files via upload

---
+++ bootstrap/KT1.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head>
+  <body>
+        <style>
+            .jumbo{
+                height: 700px;
+                width: 100%;
+                background-color: #666666;
+                background-image: url('https://picsum.photos/1752/804');
+                background-position: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+                color: #fff;
+                text-align: center;
+    
+        }
+        .card-img-top {
+        width: 100%;
+        height: 15vw;
+        object-fit: cover;
+        }
+        i {
+        background-color: #eee;
+        border-radius: 50%;
+        border: 1px grey;
+        padding: 10px;
+        }
+        </style>
+</head>
+
+    <body>
+          <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+              <a class="px-2 fa-lg fb-ic"><i class="fab fa-facebook"></i></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Templates</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                  </li>
+                </ul>
+                <div class="padding-3">
+                    <i class="fa-brands fa-instagram" class="btn btn-outline-secondary"></i>
+                    <i class="fa-brands fa-twitter" class="btn btn-outline-secondary"></i>
+                    <i class="fa-brands fa-linkedin" class="btn btn-outline-secondary"></i>
+                    </div>
+                <form class="d-flex" role="search">
+                  <button class="btn btn-outline-secondary" type="submit">Button</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+            <div class="bg-light">
+            <div class="p-5 mb-4 bg-light rounded-0 jumbo">
+            <div class="container-fluid py-5">
+            <h1 class="display-1 text-dark">Hero title top center</h1>
+            <p class="col fs-4 lead">Sub-heading. Header 100% height with bottom align items</p></div>
+            <form class="text-center" role="search">
+                  <button class="btn bg-primary text-white fs-4" type="submit">Get Started Now></button>
+                </form>
+            </div>
+            <div class="container">
+                <p class="text-primary">B l u e p r i n t s a p p<p>
+                <div class="col-8">
+                <h2>Bootstrap 4, Fontawesome and bootstwatch - all in one simple to use app interface</h2>
+                <p class="lead">Your clients have a hard time imagining how a site is going to work</p>
+            </div>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                  <div class="card h-100">
+                    <div class="card-body">
+                    <i class="fa-solid fa-droplet text-primary fs-4"></i>
+                      <h5 class="card-title">RESPONSIVE</h5>
+                      <h6 class="card-subtitle"></h6>
+                      <p class="card-text">Praesent mollis, massa in auctor molestie</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card h-100">
+                    <div class="card-body">
+                    <div class="col-3"> 
+                    <i class="fa-solid fa-lock text-primary fs-4"></i></div>
+                    <div class="col-9">
+                      <h5 class="card-title">BOOTSTRAP 4</h5>
+                      <h6 class="card-subtitle"></h6>
+                      <p class="card-text">Praesent mollis, massa in auctor molestie</p></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card h-100">
+                    <div class="card-body">
+                    <i class="fa-solid fa-mobile text-primary fs-4"></i>
+                      <h5 class="card-title text">READY TO USE</h5>
+                      <h6 class="card-subtitle"></h6>
+                      <p class="card-text">Praesent mollis, massa in auctor molestie</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="bg-light">
+        <form class="text-center" role="search">
+                  <button class="btn bg-primary text-white fs-5 mt-5 mb-5" type="submit">Lorem ipsum</button>
+                </form>
+    </div>
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+    </body>
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+  </body>
+</html>
